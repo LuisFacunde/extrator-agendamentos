d8c10ff94da8124e8ef0e2735ba3e4a952daa931
Luis Facunde <luisfacunde19@gmail.com>
2026-06-01 09:44:22 -0300

    feat: ajuste da query para retorno do campo de observação

--- src/services/oracleService.ts
+++ src/services/oracleService.ts
@@ -8,7 +8,25 @@ export interface Paciente {
 export async function fetchPacientes(limit: number): Promise<Paciente[]> {
     return withConnection(async (connection) => {
         const result = await connection.execute<Paciente>(
-            `SELECT cd_paciente AS prontuario, nm_paciente AS nome, to_date(dt_nascimento) AS data_nascimento FROM paciente WHERE ROWNUM <= :limit`,
+            `SELECT * FROM (
+                SELECT pdc.cd_atendimento AS Atendimento,
+                        p.cd_paciente AS prontuario,
+                        p.nm_paciente AS paciente,
+                        TO_CHAR(TRUNC(pdc.dh_criacao), 'DD/MM/YYYY') AS data_criacao,
+                        erc.lo_valor AS observacao
+                FROM pw_documento_clinico pdc
+                    JOIN pw_editor_clinico pec ON pdc.cd_documento_clinico = pec.cd_documento_clinico
+                    JOIN editor_registro_campo erc ON pec.cd_editor_registro = erc.cd_registro
+                    JOIN editor_campo ec ON erc.cd_campo = ec.cd_campo
+                    JOIN atendime a ON pdc.cd_atendimento = a.cd_atendimento
+                    JOIN paciente p ON a.cd_paciente = p.cd_paciente
+                WHERE pdc.tp_status = 'FECHADO'
+                    AND pec.cd_documento = 583
+                    AND ec.ds_identificador IN ('obs_2229_1')
+                    AND erc.lo_valor IS NOT NULL
+                    AND pdc.dh_criacao >= ADD_MONTHS(SYSDATE, -12)
+                ORDER BY pdc.dh_criacao ASC
+            ) WHERE ROWNUM <= :limit`,
             { limit },
             {
                 outFormat: oracledb.OUT_FORMAT_OBJECT,
